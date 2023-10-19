01169f9c30028e4b407767f321e174f649e54796
Verdy1488 <55031071+Verdy1488@users.noreply.github.com>
2023-10-19 21:09:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import telebot
+import openai
+from telebot import types
+
+token = '6654086918:AAGi38I450YnqeQpnuGxtyL3NOt1WAj770U'
+openai_token = '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'
+
+bot = telebot.TeleBot(token)
+openai.api_key = openai_token
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!')
+
+@bot.message_handler(commands=['ask'])
+def ask(message):
+    text = message.text[4:]
+
+    prompt = [{'role': 'user', 'content': text}]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=prompt
+    )
+
+    bot.send_message(message.chat.id, f"{response['choices'][0]['message']['content']}")
+
+bot.infinity_polling()
